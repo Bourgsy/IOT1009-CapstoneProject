e71b7132002d9e7431bda8d270d0c64d66f21fce
Nick Bourguignon <nbourguignon1996@gmail.com>
2024-12-10 23:07:28 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NicksApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
